81d3f8b4a509d78aefd0757e2c2ec0fbaeae6e89
KUNAL3369 <kunal.prabhakar3369@gmail.com>
2025-08-15 07:18:44 +0545

    Fix nhost export error

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -1,6 +1,6 @@
 import { NhostClient } from '@nhost/nhost-js';
 
-const nhost = new NhostClient({
+export const nhost = new NhostClient({
   subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN || 'yvmegydgacgqiqxlffrr',
   region: import.meta.env.VITE_NHOST_REGION || 'ap-south-1',
 });
